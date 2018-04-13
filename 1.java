ba7f5714e189e1a90843f99271764aa4f291d96f
gangadhar <ganga0534@gmail.com>
2018-04-12 17:48:26 -0700

    3 commit

---
+++ 1.java
@@ -0,0 +1,26 @@
+line 1
+hallo manisha hi hi 
+hi  ganga 
+hi anusha
+:wq
+Experience/knowledge of Windows or Linux systems administration
+
+Coding and/or scripting exposure level (Python/Ruby/Perl/Shell/PowerShell)
+
+knowledge on CI and CD concepts
+
+Experience/knowledge level on any of the tool (chef/Puppet/Salt/Octopus Deploy/Ansible)
+
+Experience/Knowledge level on any of the tool (Jenkins/ Bamboo/ Team City/VSTS/TFS)
+
+Experience/knowledge level on any of the tool (Jira/Redmine)
+
+Experience/knowledge level on Bit bucket or GIT
+
+Experience/Knowledge on Development of automation solutions
+
+Experience/knowledge AWS/Azure based deployments
+
+Experience/knowledge of networking, security, firewalls and infrastructure
+
+Experience in problem solving:
